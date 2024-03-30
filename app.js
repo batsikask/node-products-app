7d3d32611ac2659121bc88aee61087a3a50d61db
konst-mp <kon.batsikas@gmail.com>
2024-03-30 17:05:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+            console.log("Connection to MongoDB established"),
+            err => {console.log("Failed to connect to MongoDB, err")}
+        })
+
+const user = require('./routes/user.route');
+
+app.use('/api/users', user);
+
+app.listen(port, ()=> {
+    console.log('Server is running');
+})
