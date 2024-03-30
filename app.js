157804de3af02e46e13706aef3a1919cd79befc5
konst-mp <kon.batsikas@gmail.com>
2024-03-31 01:51:13 +0200

    cors and basic front-end

--- app.js
+++ app.js
@@ -14,10 +14,18 @@ mongoose.connect(process.env.MONGODB_URI)
             err => {console.log("Failed to connect to MongoDB, err")}
         })
 
+const cors = require('cors')
+app.use(cors({
+  origin: "*"
+  // origin:["http://localhost:8000", "http://localhost:3000"]
+}))
+
 const user = require('./routes/user.route')
 const product = require('./routes/product.route')
 const userProduct = require('./routes/user.products.route')
 
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static('files'))
 app.use('/api/users', user)
 app.use('/api/products', product)
 app.use('/api/user-products', userProduct)
